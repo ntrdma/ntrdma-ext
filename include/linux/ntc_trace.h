b23a880022ea1e7bf655b8ef7a42a55df6ccf245
Alexander Barabash <Alexander.Barabash@emc.com>
2019-10-27 15:35:35 +0200

    ntrdma: performance measurement.

--- include/linux/ntc_trace.h
+++ include/linux/ntc_trace.h
@@ -33,6 +33,11 @@
 #ifndef NTC_TRACE_H
 #define NTC_TRACE_H
 
+#include <linux/ftrace.h>
+#include <linux/timex.h>
+#include <linux/types.h>
+#include <linux/smp.h>
+
 #define TRACE_EN
 
 #ifdef TRACE_EN
@@ -55,5 +60,73 @@
 #else
 #define TRACE_DATA(...) do {} while (0)
 #endif
+
+struct ntc_perf_tracker {
+	cycles_t total;
+	cycles_t total_out;
+	cycles_t last;
+	u64 num_calls;
+};
+
+struct ntc_perf_tracker_current {
+	struct ntc_perf_tracker *tracker;
+	cycles_t start;
+	const char *prefix;
+	u64 window;
+};
+
+static inline cycles_t ntc_get_cycles(void)
+{
+	asm volatile ("":::"memory");
+	return get_cycles();
+}
+
+static inline void ntc_perf_finish_measure(struct ntc_perf_tracker_current *c)
+{
+	struct ntc_perf_tracker *t = c->tracker;
+
+	if (likely(t->last || t->num_calls))
+		t->total_out += c->start - t->last;
+
+	t->num_calls++;
+	t->last = ntc_get_cycles();
+	t->total += t->last - c->start;
+
+	if (t->num_calls != c->window)
+		return;
+
+	pr_info("PERF: %s [%d]: %lld calls. %lld%% of time. %lld cyc average.",
+		c->prefix, smp_processor_id(), t->num_calls,
+		t->total * 100 / (t->total + t->total_out),
+		t->total / t->num_calls);
+
+	t->num_calls = 0;
+	t->total_out = 0;
+	t->total = 0;
+}
+
+#define NTC_PERF_TRACK
+
+#ifdef NTC_PERF_TRACK
+
+#define DEFINE_NTC_PERF_TRACKER(name, p, w)				\
+	static DEFINE_PER_CPU(struct ntc_perf_tracker, name##_per_cpu);	\
+	struct ntc_perf_tracker_current name =				\
+	{ .start = ntc_get_cycles(), .prefix = p, .window = w }
+
+#define NTC_PERF_MEASURE(name) do {					\
+		name.tracker = get_cpu_ptr(&name##_per_cpu);		\
+		ntc_perf_finish_measure(&name);				\
+		put_cpu_ptr(&name##_per_cpu);				\
+	} while (0)
+
+#else
+#define DEFINE_NTC_PERF_TRACKER(name, p, w) int name  __attribute__ ((unused))
+#define NTC_PERF_MEASURE(name) do {} while (0)
+#endif
+
+
+#define DEFINE_NTC_FUNC_PERF_TRACKER(name, w)		\
+	DEFINE_NTC_PERF_TRACKER(name, __func__, w)
 
 #endif
